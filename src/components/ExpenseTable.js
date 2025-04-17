890776274eaa5805478c34f41276276fdae23f37
Lisa Alusa <sayilisaalusa@gmail.com>
2025-04-17 23:03:08 +0300

    working on file

---
+++ src/components/ExpenseTable.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ExpenseTable = ({ expenses}) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expense,index)=>(
+                    <tr key={index}>
+                        <td>{expense.description}</td>
+                        
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
